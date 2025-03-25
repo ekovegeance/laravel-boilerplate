a63b4d331bd1cc3bc4a2ce0cf2c2f97ad02702cc
Eko <ekovegeance7@gmail.com>
2025-03-25 19:59:28 +0700

    Remove background image from landing page

--- resources/js/pages/landing.tsx
+++ resources/js/pages/landing.tsx
@@ -12,7 +12,6 @@ export default function Welcome() {
                 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
             </Head>
             <main>
-                <img id="background" className="absolute top-16 -left-10 w-52 max-w-[877px] animate-pulse md:w-auto" src="images/bginertia.svg" />
                 <div className="relative flex min-h-screen flex-col items-center justify-center selection:bg-red-600 selection:text-white">
                     <div className="relative w-full max-w-2xl px-6 lg:max-w-7xl">
                         <main>
