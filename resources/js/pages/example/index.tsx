541da75457b8dfbf252701e98593f5078ed0839d
Eko <ekovegeance7@gmail.com>
2025-05-07 11:32:44 +0700

    Implement pagination for examples in ExampleController and update related components

--- resources/js/pages/example/index.tsx
+++ resources/js/pages/example/index.tsx
@@ -1,32 +1,37 @@
-import Layout from '@/layouts/landing-layout';
-import { Example } from '@/types';
-import { Head, Link } from '@inertiajs/react';
-import { Button } from '@/components/ui/button';
-import ExampleCard from '@/components/example/example-card';
 import CreateExampleDialog from '@/components/example/create-example-modal';
+import ExampleCard from '@/components/example/example-card';
+import { Button } from '@/components/ui/button';
+import Layout from '@/layouts/landing-layout';
+import { Example, PaginatedData } from '@/types';
+import { Head, Link } from '@inertiajs/react';
+import SimplePaginate from '@/components/stocks/simple-paginate';
 
-export default function index({ examples }: { examples: Example[] }) {
+
+export default function index({ examples }: { examples: PaginatedData<Example> }) {
     return (
         <Layout>
             <Head title="index" />
             <div className="pt-4 md:pt-24">
-                <h2 className="text-lg font-semibold mb-4">Example Read Data</h2>
-                <div className="flex flex-row gap-2 mb-4">
+                <h2 className="mb-4 text-lg font-semibold">Example Read Data</h2>
+                <div className="mb-4 flex flex-row gap-2">
                     <Link href={route('examples.create')}>
                         <Button>Create With Page</Button>
                     </Link>
                     <CreateExampleDialog />
                 </div>
-                {examples.length === 0 && (
-                    <div className="md:pt-24 flex flex-col gap-2 justify-center items-center">
+                {examples.data.length === 0 && (
+                    <div className="flex flex-col items-center justify-center gap-2 md:pt-24">
                         <h2>Examples Read Data not found</h2>
                     </div>
                 )}
-                <div className="grid grid-cols-1 gap-4 md:grid-cols-3 mb-8">
-                    {examples.map((example) => (
+                <div className="mb-8 grid grid-cols-1 gap-4 md:grid-cols-3">
+                    {examples.data.map((example: Example) => (
                         <ExampleCard key={example.id} example={example} />
                     ))}
                 </div>
+            </div>
+            <div className="my-2">
+                <SimplePaginate currentPage={examples.current_page} totalPages={examples.last_page}/>
             </div>
         </Layout>
     );
